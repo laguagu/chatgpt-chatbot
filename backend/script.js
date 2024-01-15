030817db4bf4bb8ead66b03c4d1d61778f25aaee
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-15 19:17:50 +0200

    Add cors

--- backend/script.js
+++ backend/script.js
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 import OpenAI from "openai";
 import express, { response } from "express";
+import cors from "cors";
 config();
 
 const app = express();
@@ -9,23 +10,23 @@ const port = process.env.PORT || 3001;
 const openai = new OpenAI();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/api/chat", async (req, res) => {
   try {
-    const { input } = req.body
+    const { input } = req.body;
     const completion = await openai.chat.completions.create({
       messages: [{ role: "system", content: input }],
       model: "gpt-3.5-turbo",
     });
     console.log("Generated response:", completion.choices[0].message.content);
-    res.json({ response: completion.choices[0].message.content})
+    res.json({ response: completion.choices[0].message.content });
     console.log(completion.choices[0].message.content);
   } catch (error) {
     console.error("Someting went wrong:", error.message);
-    res.status(500).json({ error:"Server API error" })
+    res.status(500).json({ error: "Server API error" });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
