836e4779888dadc99e1774c0b0b76fb2f576ea6b
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-15 17:17:24 +0200

    Add user promp

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-import { config } from "dotenv"
-config()
-
+import { config } from "dotenv";
 import OpenAI from "openai";
+import readline from "readline";
+config();
 
 const openai = new OpenAI();
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
+const userPromp = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  console.log(completion.choices[0]);
+async function main() {
+  try {
+    userPromp.prompt();
+    userPromp.on("line", async (input) => {
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: input }],
+        model: "gpt-3.5-turbo",
+      });
+
+      console.log(completion.choices[0].message.content);
+      userPromp.prompt();
+    });
+  } catch (error) {
+    console.error("Someting went wrong", error.message);
+  }
 }
 
 main();
