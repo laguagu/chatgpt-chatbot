c2a813b0347a0f9c6b3de4c7a67ca65ae6ec8259
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-15 19:15:14 +0200

    Add express.js and POST endpoint

---
+++ backend/script.js
@@ -0,0 +1,32 @@
+import { config } from "dotenv";
+import OpenAI from "openai";
+import express, { response } from "express";
+config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const openai = new OpenAI();
+
+app.use(express.json());
+
+app.post("/api/chat", async (req, res) => {
+  try {
+    const { input } = req.body
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: input }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log("Generated response:", completion.choices[0].message.content);
+    res.json({ response: completion.choices[0].message.content})
+    console.log(completion.choices[0].message.content);
+  } catch (error) {
+    console.error("Someting went wrong:", error.message);
+    res.status(500).json({ error:"Server API error" })
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
