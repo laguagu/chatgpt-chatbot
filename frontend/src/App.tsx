e02613e083f8372655a54ea7047123dce61afa81
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-17 20:08:46 +0200

    Add functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,33 +2,42 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { Message } from "./types/chatTypes";
 
-
 function Chatbox() {
-  const [message, setMessage] = useState<Message[]>([]);
-  const [input, setUserInput] = useState<string>("");
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [userInput, setUserInput] = useState<string>("");
 
   const handeInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserInput(e.target.value)
-  }
+    setUserInput(e.target.value);
+  };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-  }
+    setMessages([...messages, { text: userInput, sender: "user" }]);
+    setMessages([...messages, { text: "api vastaus", sender: "bot" }]);
+    setUserInput("");
+  };
 
   return (
     <div className="flex flex-col h-screen bg-gray-200 p-4">
       <div className="flex-1 overflow-y-auto ">
-        <p>aasd</p>
+        {messages.map((messages, index) => (
+          <div key={index}>
+            {messages.text}
+          </div>
+        ))}
       </div>
-      <form className="flex items-center">
+      <form onSubmit={handleSubmit} className="flex items-center">
         <input
           type="text"
+          value={userInput}
           className="flex-1 border-2 p-2 rounded-md"
           placeholder="Type your message..."
-          onChange={() => {}}
+          onChange={handeInputChange}
         />
-        <button className="p-2 ml-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+        <button
+          type="submit"
+          className="p-2 ml-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+        >
           Send
         </button>
       </form>
