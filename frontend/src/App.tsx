6fcf6e97031c71c618d1d45e3bdeb64fb33dee2e
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-17 19:41:42 +0200

    Set input field and button for chat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,29 @@
-function App() {
+/* eslint-disable */
+import { useState } from "react";
+
+function Chatbox() {
+  const [message, setMessage] = useState([]);
+  const [input, setInput] = useState("");
+
+
   return (
     <div className="flex flex-col h-screen bg-gray-200 p-4">
       <div className="flex-1 overflow-y-auto ">
         <p>aasd</p>
       </div>
-      <input
-        type="text"
-        className="border p-2 rounded"
-        placeholder="Type your message..."
-      />
-      <button className="h-10 mt-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
-        Send
-      </button>
+      <form className="flex items-center">
+        <input
+          type="text"
+          className="flex-1 border-2 p-2 rounded-md"
+          placeholder="Type your message..."
+          onChange={() => {}}
+        />
+        <button className="p-2 ml-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default Chatbox;
