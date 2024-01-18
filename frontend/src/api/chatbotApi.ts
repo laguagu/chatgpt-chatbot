d74cf622e0eba82093980441fe8435929c1028a9
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-19 00:16:13 +0200

    Fix axios post request

--- frontend/src/api/chatbotApi.ts
+++ frontend/src/api/chatbotApi.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-const baseUrl = ""
+const baseUrl = "http://localhost:3000/api/chat";
 
-
-const chatbotApi = async (data: any) => {
+const chatbotApi = async (userMessage: string) => {
    try {
-    const response = await axios.post(baseUrl)
+    const response = await axios.post(baseUrl, { input: userMessage })
+
     return response.data
    } catch (error) {
     throw new Error("Chatbot API-pyynnön käsittely failasi")
