f7632004f48e18ff31cd6281632970bacf553360
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-19 00:16:55 +0200

    Set bot response to submit handler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { Message } from "./types/chatTypes";
 import clsx from "clsx";
+import chatbotApi from "./api/chatbotApi";
 
 function Chatbox() {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -11,16 +12,28 @@ function Chatbox() {
     setUserInput(e.target.value);
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setMessages((prevMessages) => [
       ...prevMessages,
       { text: userInput, sender: "user" },
-      { text: "Chatbotin vastaus", sender: "bot" },
     ]);
 
+    try {
+      // Hae chatbotin vastaus
+      const botResponse = await chatbotApi(userInput);
+      
+      // Lisää chatbotin vastaus viesti taulukkoon
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { text: botResponse.response, sender: "bot" },
+      ]);
+    } catch (error) {
+      console.error("Failed to get chatbot response");
+    }
+
     setUserInput("");
-    console.log(messages);
+
   };
 
   return (
@@ -29,9 +42,7 @@ function Chatbox() {
         {messages.map((message, index) => (
           <div
             key={index}
-            className={clsx(
-              "text-2xl items-center flex bg-slate-500",
-              {
+            className={clsx("text-2xl items-center flex bg-slate-500", {
               "text-red-500": message.sender === "user",
               "text-green-500 justify-end": message.sender === "bot",
             })}
